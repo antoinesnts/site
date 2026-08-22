02b238ddf90d2e0d27aff2d6b569d33d62dde1dc
Antoine Santos <antoinesantos@M4-Antoine-MacBook-Pro-de-Antoine.local>
2026-08-22 22:27:29 +0200

    Restore outlined social icons

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -33,7 +33,7 @@ export default function Footer() {
             rel="noreferrer noopener"
             aria-label={item.label}
           >
-            <Icone weight="fill" aria-hidden="true" />
+            <Icone weight="regular" aria-hidden="true" />
           </a>
         })}
       </nav>
