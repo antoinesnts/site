dae02ab60ba1ae7e5e9dc9dad8445d7f6477845c
Antoine Santos <antoinesantos@Mac.lan>
2026-08-25 23:18:20 +0200

    Add Cercle Bleu logo

--- app/data/content.js
+++ app/data/content.js
@@ -897,7 +897,7 @@ export const clients = [
   "Le Sémaphore", "Normandie Pays d’Auge Tourisme", "Merville-Franceville", "Zeph Control", "Seasonova",
 ];
 
-export const mediasPartenaires = ["TF1", "Public Sénat", "NOVO19", "ICI", "M6", "Artum Prod", "IUT de Lannion", "École de Journalisme de Normandie"];
+export const mediasPartenaires = ["TF1", "Public Sénat", "NOVO19", "ICI", "M6", "Artum Prod", "Cercle Bleu", "IUT de Lannion", "École de Journalisme de Normandie"];
 
 export const logosMedias = [
   { nom: "TF1", fichier: "/logos/tf1.png" },
@@ -906,6 +906,7 @@ export const logosMedias = [
   { nom: "ICI", fichier: "/logos/ici.svg" },
   { nom: "M6", fichier: "/logos/m6.png" },
   { nom: "Artum Prod", fichier: "/logos/artum-prod.png" },
+  { nom: "Cercle Bleu", fichier: "/logos/cercle-bleu.png" },
   { nom: "IUT de Lannion", fichier: "/logos/iut-lannion.png" },
   { nom: "École de Journalisme de Normandie", fichier: "/logos/ecole-journalisme-normandie.png" },
 ];
