92f623dd44a9708b6b9861afd3bd3100dfcf1a86
Antoine Santos <antoinesantos@Mac.lan>
2026-08-26 09:53:01 +0200

    Correct Aktua Prod logo

--- app/data/content.js
+++ app/data/content.js
@@ -897,7 +897,7 @@ export const clients = [
   "Le Sémaphore", "Normandie Pays d’Auge Tourisme", "Merville-Franceville", "Zeph Control", "Seasonova",
 ];
 
-export const mediasPartenaires = ["TF1", "Public Sénat", "NOVO19", "ICI", "M6", "Artum Prod", "Cercle Bleu", "IUT de Lannion", "École de Journalisme de Normandie"];
+export const mediasPartenaires = ["TF1", "Public Sénat", "NOVO19", "ICI", "M6", "Aktua Prod", "Cercle Bleu", "IUT de Lannion", "École de Journalisme de Normandie"];
 
 export const logosMedias = [
   { nom: "TF1", fichier: "/logos/tf1.png" },
@@ -905,7 +905,7 @@ export const logosMedias = [
   { nom: "NOVO19", fichier: "/logos/novo19.svg" },
   { nom: "ICI", fichier: "/logos/ici.svg" },
   { nom: "M6", fichier: "/logos/m6.png" },
-  { nom: "Artum Prod", fichier: "/logos/artum-prod.png" },
+  { nom: "Aktua Prod", fichier: "/logos/aktua-prod.png" },
   { nom: "Cercle Bleu", fichier: "/logos/cercle-bleu.png" },
   { nom: "IUT de Lannion", fichier: "/logos/iut-lannion.png" },
   { nom: "École de Journalisme de Normandie", fichier: "/logos/ecole-journalisme-normandie.png" },
